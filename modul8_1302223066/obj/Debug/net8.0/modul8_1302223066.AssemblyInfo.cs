a0d96258aec77c48791b76c790f034d76b498644
rsam-66 <samuelriyanto57@gmail.com>
2024-04-20 09:07:52 +0700

    aplikasi bank transfer dengan runtime configuration - not finished

--- modul8_1302223066/obj/Debug/net8.0/modul8_1302223066.AssemblyInfo.cs
+++ modul8_1302223066/obj/Debug/net8.0/modul8_1302223066.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul8_1302223066")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e05521d7b78bf17e7da7f0d10bdad989968633")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul8_1302223066")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul8_1302223066")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
